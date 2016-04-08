f7e7fcf6b4cea2b2dd46fb78d5a20064788c5008
Andrew Wei <andrew.wei92@yahoo.com>
2016-04-08 11:26:30 -0700

    dried up code

--- oop_exercise.rb
+++ oop_exercise.rb
@@ -5,51 +5,41 @@
 # 5. CHALLENGE: Change the program to give the user the choice at the end of the game to retry the cards they got wrong.
 # 6. CHALLENGE: Change the interface with better prompts, ASCII art, etc. Be as creative as you'd like!
 
-class Gamestate
-  attr_reader :no_right, :no_wrong
-
-  def initialize
-    $no_right
-    $no_wrong
-  end
-
-end
-
-
 class Card
-  attr_reader :question, :choices, :correct_answer, :no_of_tries, :user_answer_correct
+  attr_reader :question, :choices, :no_of_incorrect_tries, :user_answer_correct
 
   def initialize(question, answers)
     @question = question
     @correct_answer = answers[0]
     @choices = answers.shuffle
-    @no_of_tries = 0
-    @user_answer_correct = false
+    @no_of_incorrect_tries = 0
+    @user_answer_correct = 0
   end
 
-  def check_answer(user_answer)
-    if user_answer.downcase == @correct_answer.downcase
-      puts "Correct!"
-      @user_answer_correct = true
-    elsif @no_of_tries <= 1
-      puts "Incorrect! Try again!"
-      @no_of_tries += 1
-    else
-      puts "Incorrect!"
-      @no_of_tries += 1
+  def check_answer
+    while @no_of_incorrect_tries <= 1 && @user_answer_correct == 0
+      if gets.chomp.downcase == @correct_answer.downcase
+        puts "Correct!"
+        @user_answer_correct = 1
+      elsif @no_of_incorrect_tries <= 1
+        puts "Incorrect! Try again!"
+        @no_of_incorrect_tries += 1
+      else
+        puts "Incorrect!"
+        @no_of_incorrect_tries += 1
+      end
     end
   end
 end
 
 
 class Deck
-  attr_reader :remaining_cards, :no_right, :no_wrong, :second_attempt
+  attr_reader :remaining_cards, :no_correct_answers, :no_incorrect_answers
 
   def initialize(trivia_data)
     @cards = []
-    @no_right = 0
-    @no_wrong = 0
-    @second_attempt = []
+    @no_correct_answers = 0
+    @no_incorrect_answers = 0
 
     trivia_data.each do |question, answers|
       @cards.push(Card.new(question, answers))
@@ -63,12 +53,9 @@ class Deck
     return @cards.pop
   end
 
-  def answered_correct
-    @no_right += 1
-  end
-
-  def answered_incorrect
-    @no_wrong += 1
+  def tally_score(user_answer_correct, no_of_incorrect_tries)
+    @no_correct_answers += user_answer_correct
+    @no_incorrect_answers += no_of_incorrect_tries
   end
 end
 
@@ -79,31 +66,23 @@ trivia_data = {
 }
 
 deck = Deck.new(trivia_data) # deck is an instance of the Deck class
+# second_try = {}
 
 while deck.remaining_cards > 0
   card = deck.draw_card # card is an instance of the Card class
   puts 126.chr + 126.chr + 126.chr + 126.chr + 126.chr + 126.chr + 126.chr + 126.chr + 126.chr + 126.chr + 126.chr
   puts card.question
-  puts "Pick an answer from the following: "
+  puts "Pick an answer from the following:"
   puts card.choices
-  while card.no_of_tries <= 1 && card.user_answer_correct == false
-    card.check_answer(gets.chomp)
-    if card.user_answer_correct
-      deck.answered_correct
-    else
-      deck.answered_incorrect
-    end
-  end
+  card.check_answer
+  deck.tally_score(card.user_answer_correct, card.no_of_incorrect_tries)
+
+  # if card.no_of_incorrect_tries == 2
+
+  # end
 end
 
-puts "You answered #{deck.no_right} questions correctly and had #{deck.no_wrong} incorrect attempts. Thanks for playing!"
-
-
-
-
-
-
-
+puts "You answered #{deck.no_correct_answers} questions correctly and had #{deck.no_incorrect_answers} incorrect attempts. Thanks for playing!"
 
 
 
